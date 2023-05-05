be84e4e4b7a94d2a6a6ecc2e6b4629f4ee267ba5
Jacob George <kooldrjay060@gmail.com>
2023-05-05 12:58:31 +0100

    changed schedule test from everyminute to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:website-email')->everyMinute();
+        $schedule->command('send:website-email')->daily();
     }
 
     /**
